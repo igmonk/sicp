b6786f34c6af85b8ebc269a5afe858245a8b16b3
Igor Moiseyenko <imoiseyenko93@gmail.com>
2021-08-01 00:20:44 +0200

    Exercise 1.11: fix for negative numbers.

--- chapter1/1.2/ex-1.11.scm
+++ chapter1/1.2/ex-1.11.scm
@@ -16,38 +16,54 @@
 	 (* 2 (f (- n 2)))
 	 (* 3 (f (- n 3))))))
 
-;; (f 0) ; 0
-;; (f 1) ; 1
-;; (f 2) ; 2
+;; (f -100) ; -100
+;; (f -10)  ; -10
+;; (f 0)    ; 0
+;; (f 1)    ; 1
+;; (f 2)    ; 2
+
 ;; (f 3) ; 4
 ;; (f 4) ; 11
 ;; (f 5) ; 25
 
 ;; 2. Iterative
 ;;
-;; The idea is to use a triple of integers a, b, and c,
-;; initialized to f(0) = 0, f(1) = 1 and f(2) = 2, respectively,
-;; and to repeatedly apply the simultaneous transformations:
+;; Since the procedure describes behaviour of a piecewise function,
+;; it is reasonable to split its implementation in two, one for each range:
+;; 1) n<3
+;; 2) the rest (n>=3)
+;;
+;; Whereas, the first sub-function is as trivial as to return its argument,
+;; the second one requires the introduction of new state variables.
+;;
+;; The idea is to use a triple of integers a, b, and c, initialized to f(0) = 0,
+;; f(1) = 1 and f(2) = 2, respectively, and apply the following transformation
+;; until the counter reaches 0:
 ;; a <- b
 ;; b <- c
 ;; c <- c + 2b + 3a
 ;;
-;; After applying this transformation n times, a, b, and c
-;; will be equal, respectively, to f(n), f(n+1) and f(n+2).
+;; After applying this transformation n-2 times, a, b, and c will be equal to
+;; f(n-2), f(n-1) and f(n), respectively.
 ;;
 ;; Thus, we can compute f(n) iteratively using the procedure:
 
 (define (f n)
-  (f-iter 0 1 2 n))
+  (if (< n 3)
+      n
+      (f-iter 0 1 2 (- n 2))))
 
-(define (f-iter a b c n)
-  (if (> n 0)
-      (f-iter b c (+ c (* 2 b) (* 3 a)) (- n 1))
-      a))
+(define (f-iter a b c count)
+  (if (= count 0)
+      c
+      (f-iter b c (+ c (* 2 b) (* 3 a)) (- count 1))))
 
-;; (f 0) ; 0
-;; (f 1) ; 1
-;; (f 2) ; 2
-;; (f 3) ; 4
-;; (f 4) ; 11
-;; (f 5) ; 25
+;; (f -100) ; -100
+;; (f -10)  ; -10
+;; (f 0)    ; 0
+;; (f 1)    ; 1
+;; (f 2)    ; 2
+
+;; (f 3)  ; 4
+;; (f 4)  ; 11
+;; (f 5)  ; 25
