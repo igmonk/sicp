742652a782b178d9be1eb4f98b881bddf15f1e92
Igor Moiseyenko <imoiseyenko93@gmail.com>
2021-08-06 22:58:17 +0200

    Exercise 1.30

---
+++ chapter1/1.3/ex-1.30.scm
@@ -0,0 +1,28 @@
+;; Exercise 1.30
+;;
+;; The sum procedure above generates a linear recursion.
+;; The procedure can be rewritten so that the sum is performed iteratively.
+;; Show how to do this by filling in the missing expressions in the following definition:
+;;
+;; (define (sum term a next b)
+;;   (define (iter a result)
+;;     (if <??>
+;;         <??>
+;;         (iter <??> <??>)))
+;;   (iter <??> <??>))
+
+(define (sum term a next b)
+  (define (iter a result)
+    (if (> a b)
+	result
+	(iter (next a) (+ (term a) result))))
+  (iter a 0))
+
+(define (identity x) x)
+
+(define (inc n) (+ n 1))
+
+(define (sum-integers a b)
+  (sum identity a inc b))
+
+;; (sum-integers 1 10) ; 55
