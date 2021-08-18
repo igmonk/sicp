51c7b7ccb144b6c6c62edac92e5cbfe4cacda644
Igor Moiseyenko <imoiseyenko93@gmail.com>
2021-08-18 18:09:36 +0200

    Exercise 1.34

---
+++ chapter1/1.3/ex-1.34.scm
@@ -0,0 +1,27 @@
+;; Exercise 1.34
+;;
+;; Suppose we define the procedure
+
+(define (f g)
+  (g 2))
+
+;; Then we have
+;;
+;; (f square)                     ; 4
+;; (f (lambda (z) (* z (+ z 1)))) ; 6
+;;
+;; What happens if we (perversely) ask the interpreter to evaluate the combination (f f)? Explain.
+
+;; During evaluation of (f f), the interpreter makes another invocation of f in the form (f 2),
+;; since the variable g is bound to f.
+;; In turn, (f 2) makes the interpreter bind g to 2 and try to evaluate (2 2), which fails
+;; due to the inapplicability of a number.
+;;
+;; The exact message returned by the interpreter:
+;; The object 2 is not applicable.
+
+
+;; Utils
+
+(define (square x)
+  (* x x))
