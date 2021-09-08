19939c9b3aceba3d548ed6c5306ba486573293c6
Igor Moiseyenko <imoiseyenko93@gmail.com>
2021-09-08 20:27:18 +0200

    A namespace containing globally-used definitions.

---
+++ common.scm
@@ -0,0 +1,13 @@
+;; Some common definitions used globally across all the chapters.
+
+(define (square x)
+  (* x x))
+
+(define (cube x)
+  (* x x x))
+
+(define (inc n)
+  (+ n 1))
+
+(define (dec n)
+  (- n 1))
