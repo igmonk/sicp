7f4d5f225f9675135da84c3506e50715a2b69ec0
Igor Moiseyenko <imoiseyenko93@gmail.com>
2021-09-10 20:57:57 +0200

    Exercise 2.28

---
+++ chapter2/2.2/ex-2.28.scm
@@ -0,0 +1,41 @@
+;; Exercise 2.28
+;;
+;; Write a procedure 'fringe' that takes as argument a tree (represented as a list) and
+;; returns a list whose elements are all the leaves of the tree arranged in left-to-right order.
+
+
+;; 1. Iterative
+
+(define (fringe x)
+  (define (iter l result)
+    (cond ((null? l) result)
+          ((not (pair? l)) (list l))
+          (else (append (iter (car l) result)
+                        (iter (cdr l) result)))))
+  (iter x '()))
+
+
+;; 2. Recursive
+
+(define (fringe x)
+  (cond ((null? x) x)
+        ((not (pair? x)) (list x))
+        (else (append (fringe (car x))
+                      (fringe (cdr x))))))
+
+
+(define x (list (list 1 2) (list 3 4)))
+
+;; x          ; ((1 2) (3 4))
+;; (fringe x) ; (1 2 3 4)
+
+(define xx (list (list 1 2) (list 3 4) (list 5 6)))
+
+;; xx          ; ((1 2) (3 4) (5 6))
+;; (fringe xx) ; (1 2 3 4 5 6)
+
+(define xxx (list (list (list 1 2) (list 3 4))
+                  (list (list 5 6) (list 7 8))))
+
+;; xxx          ; (((1 2) (3 4)) ((5 6) (7 8)))
+;; (fringe xxx) ; (1 2 3 4 5 6 7 8)
