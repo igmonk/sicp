cee00ee7080592f702982cdeea6b40370ffdfb48
Igor Moiseyenko <imoiseyenko93@gmail.com>
2021-10-04 22:18:13 +0200

    Some globally used functions.

--- common.scm
+++ common.scm
@@ -13,3 +13,22 @@
   (- n 1))
 
 (define (identity x) x)
+
+(define (fib-iter a b n)
+  (if (> n 0)
+      (fib-iter (+ a b) a (- n 1))
+      b))
+
+(define (fib n)
+  (fib-iter 1 0 n))
+
+(define (prime? n)
+  (not (div-iter n 2)))
+
+(define (div-iter n divisor)
+  (cond ((< n (square divisor)) false)
+	((divisible? n divisor) true)
+	(else (div-iter n (+ divisor 1)))))
+
+(define (divisible? n divisor)
+  (= (remainder n divisor) 0))
