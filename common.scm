8d947c7c18c558e1d67ea34b61d56048c54e616c
Igor Moiseyenko <imoiseyenko93@gmail.com>
2021-10-25 20:19:00 +0200

    Make compose and repeated globally available.

--- common.scm
+++ common.scm
@@ -46,3 +46,12 @@
          (if (equal? (car seq) (cadr seq))
              (all-equal? (cdr seq))
              false))))
+
+(define (compose f g)
+  (lambda (x)
+    (f (g x))))
+
+(define (repeated f n)
+  (if (> n 0)
+      (compose f (repeated f (- n 1)))
+      identity))
