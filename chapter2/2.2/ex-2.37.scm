5ca2bfd8da25d9e08717c970fded04ed08057f71
Igor Moiseyenko <imoiseyenko93@gmail.com>
2021-11-20 18:55:43 +0100

    ex-2.37: comment out value definitions.

--- chapter2/2.2/ex-2.37.scm
+++ chapter2/2.2/ex-2.37.scm
@@ -59,11 +59,11 @@
 (define (matrix-*-vector m v)
   (map (lambda (m-row) (dot-product m-row v)) m))
 
-(define m1 (list (list 1 2 3 4)
-                 (list 4 5 6 6)
-                 (list 6 7 8 9)))
+;; (define m1 (list (list 1 2 3 4)
+;;                  (list 4 5 6 6)
+;;                  (list 6 7 8 9)))
 
-(define v1 (list 1 3 5 7))
+;; (define v1 (list 1 3 5 7))
 
 ;; (matrix-*-vector m1 v1) ; (50 91 130)
 
