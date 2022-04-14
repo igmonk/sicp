fdf931f7855a34f60128bb6cc8b72a211f22baeb
Igor Moiseyenko <imoiseyenko93@gmail.com>
2022-04-14 09:37:54 +0200

    Interfacing compiled code to the evaluator.

--- chapter5/5.5/tests.scm
+++ chapter5/5.5/tests.scm
@@ -17,3 +17,30 @@
 
 ;; Save to a file if needed.
 (instruction-seq->file compiled-factorial "factorial.obj")
+
+
+;; Compile and go
+(load "compile-and-go.scm")
+
+(compile-and-go
+ '(define (factorial n)
+    (if (= n 1)
+        1
+        (* (factorial (- n 1)) n))))
+
+(total-pushes = 0 maximum-depth = 0)
+;;; EC-Eval value:
+ok
+
+;;; EC-Eval input:
+(factorial 5)
+
+(total-pushes = 31 maximum-depth = 14)
+;;; EC-Eval value:
+120
+
+;; The interpreted version required 144 pushes and
+;; a maximum stack depth of 28!
+;;
+;; This illustrates the optimization that results from
+;; the compilation strategy.
