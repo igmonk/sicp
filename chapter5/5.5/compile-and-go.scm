c7faacfd2d00af6ac9e0428d0b7d561c98f3ca18
Igor Moiseyenko <imoiseyenko93@gmail.com>
2022-04-18 11:59:06 +0200

    Make use of the extended eval machine.

--- chapter5/5.5/compile-and-go.scm
+++ chapter5/5.5/compile-and-go.scm
@@ -2,9 +2,9 @@
 
 (load "../5.2/basic-machine-ext.scm")
 
-(load "../5.4/evaluator-machine.scm")
 (load "../5.4/env-ext.scm")
 
+(load "eval-machine.scm")
 (load "instruction-seq.scm")
 
 ;; Note: the following error might get thrown (depends on
@@ -17,13 +17,13 @@
 ;; ../5.2/assembler.scm).
 
 (define (assemble-object-code object-code)
-  (((ec-eval-machine 'assembler) 'assemble) object-code))
+  (((ec-eval-ext-machine 'assembler) 'assemble) object-code))
 
 ;; Requires 'compile' to be defined
 (define (compile-and-go exp)
   (let* ((object-code (statements (compile exp 'val 'return)))
          (instructions (assemble-object-code object-code)))
     (set! the-global-environment (setup-environment))
-    (set-register-contents! ec-eval-machine 'val instructions)
-    (set-register-contents! ec-eval-machine 'flag true)
-    (start ec-eval-machine)))
+    (set-register-contents! ec-eval-ext-machine 'val instructions)
+    (set-register-contents! ec-eval-ext-machine 'flag true)
+    (start ec-eval-ext-machine)))
--- chapter5/5.5/tests.scm
+++ chapter5/5.5/tests.scm
@@ -28,14 +28,14 @@
         1
         (* (factorial (- n 1)) n))))
 
-(total-pushes = 0 maximum-depth = 0)
+;; (total-pushes = 0 maximum-depth = 0)
+
 ;;; EC-Eval value:
 ok
 
 ;;; EC-Eval input:
-(factorial 5)
+(factorial 5) ;; (total-pushes = 31 maximum-depth = 14)
 
-(total-pushes = 31 maximum-depth = 14)
 ;;; EC-Eval value:
 120
 
