a87ff0081a9750a121a773b4fd0adc882729925c
Igor Moiseyenko <imoiseyenko93@gmail.com>
2022-04-19 20:04:45 +0200

    Compile-and-run tests.

--- chapter5/5.5/tests.scm
+++ chapter5/5.5/tests.scm
@@ -44,3 +44,62 @@ ok
 ;;
 ;; This illustrates the optimization that results from
 ;; the compilation strategy.
+
+
+
+;; Compile and run
+(load "compile-and-run.scm")
+
+;; Add the primitive to the list of primitive procedures
+;; of the machine and reset the global environment to
+;; reinitialize the machine's primitive procedures:
+
+(append! primitive-procedures
+         (list (list 'compile-and-run compile-and-run)))
+
+(reset-env!)
+
+
+;; Set flag to false and re-run the machine:
+
+(set-register-contents! ec-eval-ext-machine 'flag false)
+(start ec-eval-ext-machine)
+
+
+;;; EC-Eval input:
+(compile-and-run
+ '(define (factorial n)
+    (if (= n 1)
+        1
+        (* (factorial (- n 1)) n))))
+
+(total-pushes = 6 maximum-depth = 3)
+;;; EC-Eval value:
+ok
+
+;;; EC-Eval input:
+(factorial 5)
+
+(total-pushes = 31 maximum-depth = 14)
+;;; EC-Eval value:
+120
+
+;;; EC-Eval input:
+(compile-and-run
+ '(define (fib n)
+    (if (< n 2)
+        n
+        (+ (fib (- n 1)) (fib (- n 2))))))
+
+(total-pushes = 6 maximum-depth = 3)
+;;; EC-Eval value:
+ok
+
+;;; EC-Eval input:
+(fib 10)
+
+(total-pushes = 887 maximum-depth = 29)
+;;; EC-Eval value:
+55
+
+;;; EC-Eval input:
